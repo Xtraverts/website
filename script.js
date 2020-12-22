2dfba3643e3f5b7d77707355f2e3a15779ec05d5
Massimiliano Bisceglie <maxbisceglie89@gmail.com>
2020-12-22 01:48:09 +0100

    testing flickering

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ function bgOff() {
 
 setTimeout(bgOff, 2000);
 setTimeout(bgOn, 2400);
-setTimeout(bgOff, 2600);
-setTimeout(bgOn, 2700);
-setTimeout(bgOff, 2880);
-setTimeout(bgOn, 3000);
+setTimeout(bgOff, 2800);
+setTimeout(bgOn, 2980);
+setTimeout(bgOff, 3160);
+setTimeout(bgOn, 3280);
