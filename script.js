c54aadb5a1852df845a94af7d40f98fe72e07cb5
Massimiliano Bisceglie <maxbisceglie89@gmail.com>
2020-12-22 01:46:08 +0100

    flickering fixed

--- script.js
+++ script.js
@@ -1,21 +1,19 @@
 const bg = document.querySelector('body');
-bg.classList.add('bg_off');
-bg.classList.remove('bg_off');
 bg.classList.add('bg_on');
 
 function bgOn() {
-  bg.classList.add('bg_on');
   bg.classList.remove('bg_off');
+  bg.classList.add('bg_on');
 };
 
 function bgOff() {
-  bg.classList.remove('bg_on');
   bg.classList.add('bg_off');
+  bg.classList.remove('bg_on');
 };
 
 setTimeout(bgOff, 2000);
-setTimeout(bgOn, 2200);
-setTimeout(bgOff, 2500);
+setTimeout(bgOn, 2400);
+setTimeout(bgOff, 2600);
 setTimeout(bgOn, 2700);
-
-console.log('test')
+setTimeout(bgOff, 2880);
+setTimeout(bgOn, 3000);
