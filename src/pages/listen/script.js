a614a4571a5483c33e263056729818142d3a0775
Massimiliano Bisceglie <maxbisceglie89@gmail.com>
2021-02-10 01:23:43 +0000

    added songs

--- src/pages/listen/script.js
+++ src/pages/listen/script.js
@@ -1,4 +1,9 @@
-const sample = '../../assets/audio/sample.mp3';
+const song_speed = '../../assets/audio/speed.mp3';
+const song_johhny = '../../assets/audio/johnny.mp3';
+const song_1984 = '../../assets/audio/1984.mp3';
+const song_plastic = '../../assets/audio/plastic.mp3';
+const song_blank_generation = '../../assets/audio/blank-generation.mp3';
+const song_police_state = '../../assets/audio/police-state.mp3';
 
 const player = document.getElementById('player');
 
@@ -11,34 +16,48 @@ const deselectElement = (element, event) => {
   event.target.classList.add('selected-song');
 }
 
+let audio;
+
+const handleAudio = (song) => {
+  if (audio) {
+    audio.pause();
+  }
+  audio = new Audio(song);
+  audio.load();
+  audio.play();
+}
 
 player.addEventListener('click', (e) => {
   switch (e.target.id) {
     
     case 'song-1':
       deselectElement(player.childNodes, e);
-      let audio = new Audio(sample);
-      audio.play();
+      handleAudio(song_speed);
       break;
 
     case 'song-2':
       deselectElement(player.childNodes, e);
+      handleAudio(song_johhny);
       break;
 
     case 'song-3':
       deselectElement(player.childNodes, e);
+      handleAudio(song_1984);
       break;
 
     case 'song-4':
       deselectElement(player.childNodes, e);
+      handleAudio(song_plastic);
       break;
 
     case 'song-5':
       deselectElement(player.childNodes, e);
+      handleAudio(song_blank_generation);
       break;
 
     case 'song-6':
       deselectElement(player.childNodes, e);
+      handleAudio(song_police_state);
       break;
 
     default:
